5b00bbfd5a2614c7f5edc3ad5abe376892a8d39f
keiyakanno <keiya@kanno.com>
2015-08-17 14:11:22 +0000

    have_and_want_btn

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -11,7 +11,10 @@ class OwnershipsController < ApplicationController
     # itemsテーブルに存在しない場合はAmazonのデータを登録する。
     if @item.new_record?
       # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-      response = {}
+      response = Amazon::Ecs.item_search(params[:asin] , 
+                                  :search_index => 'All' , 
+                                  :response_group => 'Medium' , 
+                                  :country => 'jp')
       amazon_item       = response.items.first
       @item.title        = amazon_item.get('ItemAttributes/Title')
       @item.small_image  = amazon_item.get("SmallImage/URL")
@@ -25,7 +28,13 @@ class OwnershipsController < ApplicationController
     # TODO ユーザにwant or haveを設定する
     # params[:type]の値ににHaveボタンが押された時にはの時は「Have」,
     # Wantボタンがされた時には「Want」が設定されています。
-    
+
+    @type = params[:type]
+    if @type == "Have"
+      current_user.have(@item)
+    elsif @type == "Want"
+      current_user.want(@item)
+    end
 
   end
 
@@ -35,6 +44,13 @@ class OwnershipsController < ApplicationController
     # TODO 紐付けの解除。 
     # params[:type]の値ににHavedボタンが押された時にはの時は「Have」,
     # Wantedボタンがされた時には「Want」が設定されています。
+
+    @type = params[:type]
+    if @type == "Have"
+      current_user.unhave(@item)
+    elsif @type == "Want"
+      current_user.unwant(@item)
+    end
 
   end
 end
